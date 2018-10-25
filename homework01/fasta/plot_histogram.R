548730a3b2bde73714bba374f8322ccedfc68a26
xenor <xenorlawl@gmail.com>
2018-10-25 08:57:47 +0200

    add plots to pdf

--- homework01/fasta/plot_histogram.R
+++ homework01/fasta/plot_histogram.R
@@ -1,3 +1,5 @@
+pdf("plots.pdf")
+
 fname = "p100.txt"
 x = read.csv(fname, header = F)
 
@@ -25,3 +27,17 @@ n = x$V1
 hash_n = x$V2
 
 barplot(height = log(hash_n), names.arg = n)
+
+# //////////////////////////////////////////////////////////////////////////
+
+fname = "data_without_canon.txt"
+x = read.csv(fname, header = F)
+
+p = x[[1]]
+fpr = x[[2]]
+fnr = x[[3]]
+
+plot(  x = fpr,  y = fnr,  pch = 20,  type = "p",  xlim = c(0, 100),  ylim = c(0, 100))
+#text(fpr,     fnr,     labels = p,     cex = 0.7,     pos = 4)
+
+dev.off()
